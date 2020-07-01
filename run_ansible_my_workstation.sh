912673316b955ef6563c8f774a13269c9eb7bcc8
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-07-01 17:10:28 +0200

    Fix ansible-lint issues

--- run_ansible_my_workstation.sh
+++ run_ansible_my_workstation.sh
@@ -4,4 +4,4 @@ DESTINATION_IP="192.168.122.186"
 MYUSER="mirantis"
 PASSWORD="mirantis"
 
-ansible-playbook --diff --user="${MYUSER}" --extra-vars "ansible_password=${PASSWORD} ansible_become_password=${PASSWORD}" -i "${DESTINATION_IP}," main.yml
+ansible-playbook --skip-tags printer --diff --user="${MYUSER}" --extra-vars "ansible_password=${PASSWORD} ansible_become_password=${PASSWORD}" -i "${DESTINATION_IP}," main.yml
