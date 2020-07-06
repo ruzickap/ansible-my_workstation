6d85290a3298df6521b967002954a7c0eaa7e5f7
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-07-06 17:45:20 +0200

    Add interactive prompt before "interactive tasks" (#4)

--- run_ansible_my_workstation.sh
+++ run_ansible_my_workstation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DESTINATION_IP="192.168.122.186"
-MYUSER="mirantis"
-PASSWORD="mirantis"
+DESTINATION_IP="192.168.122.197"
+MYUSER="pruzicka"
+PASSWORD="xxxx"
 
 ansible-playbook --skip-tags printer --diff --user="${MYUSER}" --extra-vars "ansible_password=${PASSWORD} ansible_become_password=${PASSWORD}" -i "${DESTINATION_IP}," main.yml
