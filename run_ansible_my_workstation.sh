6419cefede617e1642015b2b5e9701c0ba1e5ad2
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-07-08 11:36:15 +0200

    Improve ansible.cfg to print handy stats (#5)

--- run_ansible_my_workstation.sh
+++ run_ansible_my_workstation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DESTINATION_IP="192.168.122.197"
+DESTINATION_IP="172.19.84.77"
 MYUSER="pruzicka"
 PASSWORD="xxxx"
 
-ansible-playbook --skip-tags printer --diff --user="${MYUSER}" --extra-vars "ansible_password=${PASSWORD} ansible_become_password=${PASSWORD}" -i "${DESTINATION_IP}," main.yml
+ansible-playbook --skip-tags printer,secrets,user_specific --diff --user="${MYUSER}" --extra-vars "ansible_password=${PASSWORD} ansible_become_password=${PASSWORD}" -i "${DESTINATION_IP}," main.yml
