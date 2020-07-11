b46e11e0aa75f1a5abe4c01e03abc7afc2f8cd66
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-07-11 09:54:36 +0200

    Clean Ansible code and GH Actions (#8)

--- run_ansible_my_workstation.sh
+++ run_ansible_my_workstation.sh
@@ -4,4 +4,4 @@ DESTINATION_IP="172.19.84.77"
 MYUSER="pruzicka"
 PASSWORD="xxxx"
 
-ansible-playbook --skip-tags printer,secrets,user_specific --diff --user="${MYUSER}" --extra-vars "ansible_password=${PASSWORD} ansible_become_password=${PASSWORD}" -i "${DESTINATION_IP}," main.yml
+ansible-playbook --skip-tags secrets,test_data --diff --user="${MYUSER}" --extra-vars "ansible_password=${PASSWORD} ansible_become_password=${PASSWORD}" -i "${DESTINATION_IP}," main.yml
