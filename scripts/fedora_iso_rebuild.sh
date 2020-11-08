4e98cdfd19b2836b7d9351834fea1f226d281528
Petr Ruzicka <petr.ruzicka@gmail.com>
2020-11-08 19:56:03 +0100

    Add Fedora 33, remove RPM Fusion GPG keys

--- scripts/fedora_iso_rebuild.sh
+++ scripts/fedora_iso_rebuild.sh
@@ -3,26 +3,23 @@
 set -eux
 
 TMP_ISO="/tmp/iso"
-TMP_ISO2="/tmp/iso2"
 DESTINATION="${HOME}/Documents/iso/"
-ISO_SOURCE="${HOME}/Documents/iso/Fedora-Server-netinst-x86_64-32-1.6.iso"
+ISO_SOURCE="${DESTINATION}/Fedora-Server-netinst-x86_64-33-1.2.iso"
 VOLUME_LABEL=$(isoinfo -d -i "${ISO_SOURCE}" | awk -F': ' '/Volume id:/ { print $2 }')
 BOOT_PARAMS="inst.ks=hd:LABEL=${VOLUME_LABEL}:/fedora-ks.cfg"
 DESTINATION_FILE_NAME="$(basename "${ISO_SOURCE}" .iso)-my.iso"
 
 xorriso -osirrox on -indev "${ISO_SOURCE}" -extract / "${TMP_ISO}"
 
-mkdir "${TMP_ISO2}"
-sudo mount "${TMP_ISO}/images/efiboot.img" "${TMP_ISO2}"
-
-sudo sed -i.orig \
-  -e "s@${VOLUME_LABEL} quiet@${VOLUME_LABEL} quiet ${BOOT_PARAMS}@" \
-  -e "s@set timeout=60@# set timeout=60@" \
-  -e "s@set default=\"1\"@set default=\"0\"@" \
-${TMP_ISO}/EFI/BOOT/grub.cfg ${TMP_ISO}/EFI/BOOT/BOOT.conf ${TMP_ISO2}/EFI/BOOT/grub.cfg ${TMP_ISO2}/EFI/BOOT/BOOT.conf
-
-sudo umount "${TMP_ISO2}"
-rmdir "${TMP_ISO2}"
+# MacOS workaround - MacOS can not mount the /iso/images/efiboot.img
+docker run -t --rm -e VOLUME_LABEL -e BOOT_PARAMS -v "${TMP_ISO}:/iso" --privileged ubuntu /bin/bash -c "set -x \
+&& mount /iso/images/efiboot.img /mnt \
+&& sed -i.orig \
+     -e \"s@${VOLUME_LABEL} quiet@${VOLUME_LABEL} quiet ${BOOT_PARAMS}@\" \
+     -e \"s@set timeout=60@# set timeout=60@\" \
+     -e 's@set default=\"1\"@set default=\"0\"@' \
+   /iso/EFI/BOOT/grub.cfg /iso/EFI/BOOT/BOOT.conf /mnt/EFI/BOOT/grub.cfg /mnt/EFI/BOOT/BOOT.conf \
+&& umount /mnt"
 
 sed -i.orig \
   -e "s|${VOLUME_LABEL} quiet|${VOLUME_LABEL} quiet ${BOOT_PARAMS}|" \
@@ -35,9 +32,11 @@ tar czhf ${TMP_ISO}/fedora_workstation.tar.gz ../../ansible-my_workstation
 cp ../kickstart_file/fedora-ks.cfg "${TMP_ISO}/"
 
 test -f "${DESTINATION}/${DESTINATION_FILE_NAME}" && rm -f "${DESTINATION}/${DESTINATION_FILE_NAME}"
+# wget -c http://download.bmsoft.de/archiv/boot/syslinux/mbr/isohdpfx.bin -O /tmp/isohdpfx.bin
+dd if="${ISO_SOURCE}" of=/tmp/isohdpfx.bin bs=512 count=1
 xorriso -as mkisofs \
   -R -J -volid "${VOLUME_LABEL}" \
-  -isohybrid-mbr /usr/share/syslinux/isohdpfx.bin \
+  -isohybrid-mbr /tmp/isohdpfx.bin \
   -eltorito-catalog isolinux/boot.cat \
   -eltorito-boot isolinux/isolinux.bin \
   -no-emul-boot \
