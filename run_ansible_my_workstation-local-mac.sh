61c566fbaf91a6de25ea3b8fc440446f0399fa62
Petr Ruzicka <petr.ruzicka@gmail.com>
2021-08-02 07:01:07 +0200

    Use super-linter:slim

--- run_ansible_my_workstation-local-mac.sh
+++ run_ansible_my_workstation-local-mac.sh
@@ -3,4 +3,4 @@
 PASSWORD="my_password"
 
 set -eu
-ansible-playbook --skip-tags data,test_data,company --diff --extra-vars "ansible_password=${PASSWORD} ansible_become_password=${PASSWORD}" --connection=local -i "127.0.0.1," main.yml
+ansible-playbook --skip-tags data,test_data,company --diff --extra-vars "ansible_python_interpreter=/usr/bin/python3 ansible_password=${PASSWORD}" --connection=local -i "127.0.0.1," main.yml
