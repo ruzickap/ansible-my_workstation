998ad86dd0f35296584014c12396705187f63c12
Petr Ruzicka <petr.ruzicka@gmail.com>
2023-11-25 16:38:02 +0100

    refactor(zsh): minor zsh, powerlevel10k, mc fixes

--- run_ansible_my_workstation-local-mac.sh
+++ run_ansible_my_workstation-local-mac.sh
@@ -2,6 +2,8 @@
 
 PASSWORD="my_password"
 
-set -eu
+set -eux
 cd ansible || exit
-ansible-playbook --skip-tags data,company --diff --extra-vars "ansible_python_interpreter=/usr/bin/python3 ansible_password=${PASSWORD}" --connection=local -i "127.0.0.1," main.yml
+
+# ansible-playbook --skip-tags data,company --diff --extra-vars "ansible_python_interpreter=/usr/bin/python3 ansible_password=${PASSWORD}" --connection=local -i "127.0.0.1," main.yml
+ansible-playbook --step --start-at-task="Create directory for mc inside ~/.config" --skip-tags data,company --diff --extra-vars "ansible_python_interpreter=/usr/bin/python3 ansible_password=${PASSWORD}" --connection=local -i "127.0.0.1," main.yml
