0f6273592698e90136f44b24aa86dddc7031a22a
Petr Ruzicka <petr.ruzicka@gmail.com>
2023-12-25 20:12:48 +0700

    feat(gha): mega-linter config file, add opentofu, remove plugins

--- run_ansible_my_workstation-local-mac.sh
+++ run_ansible_my_workstation-local-mac.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-PASSWORD="my_password"
+MY_PASSWORD=""
 
 set -eux
 cd ansible || exit
 
-# ansible-playbook --skip-tags data,company --diff --extra-vars "ansible_python_interpreter=/usr/bin/python3 ansible_password=${PASSWORD}" --connection=local -i "127.0.0.1," main.yml
-ansible-playbook --step --start-at-task="Create directory for mc inside ~/.config" --skip-tags data,company --diff --extra-vars "ansible_python_interpreter=/usr/bin/python3 ansible_password=${PASSWORD}" --connection=local -i "127.0.0.1," main.yml
+# ansible-playbook --skip-tags data,company --diff --extra-vars "ansible_python_interpreter=/usr/bin/python3 ansible_password=${MY_PASSWORD}" --connection=local -i "127.0.0.1," main.yml
+ansible-playbook --step --start-at-task="Create directory for mc inside ~/.config" --skip-tags data,company --diff --extra-vars "ansible_python_interpreter=/usr/bin/python3 ansible_password=${MY_PASSWORD}" --connection=local -i "127.0.0.1," main.yml
