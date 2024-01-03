83e4685b5357c6a0fda363ed74671faf5b3bf2b5
Petr Ruzicka <petr.ruzicka@gmail.com>
2024-01-03 13:48:24 +0100

    feat(zsh): changes to ZSH_HIGHLIGHT_MAXLENGTH parameter

--- run_ansible_my_workstation-local-mac.sh
+++ run_ansible_my_workstation-local-mac.sh
@@ -5,5 +5,4 @@ MY_PASSWORD=""
 set -eux
 cd ansible || exit
 
-# ansible-playbook --skip-tags data,company --diff --extra-vars "ansible_python_interpreter=/usr/bin/python3 ansible_password=${MY_PASSWORD}" --connection=local -i "127.0.0.1," main.yml
-ansible-playbook --step --start-at-task="Create directory for mc inside ~/.config" --skip-tags data,company --diff --extra-vars "ansible_python_interpreter=/usr/bin/python3 ansible_password=${MY_PASSWORD}" --connection=local -i "127.0.0.1," main.yml
+ansible-playbook --step --skip-tags data,company --diff --extra-vars "ansible_python_interpreter=/usr/bin/python3 ansible_password=${MY_PASSWORD}" --connection=local -i "127.0.0.1," main.yml
