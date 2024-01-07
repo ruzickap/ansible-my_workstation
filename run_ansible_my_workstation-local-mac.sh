491600bfd27065c212d25bc15a7a46ad438b2533
Petr Ruzicka <petr.ruzicka@gmail.com>
2024-01-07 19:32:28 +0100

    feat(mac): move to MacBook Air and macOS 14.2.1
    BREAKING CHANGE:

--- run_ansible_my_workstation-local-mac.sh
+++ run_ansible_my_workstation-local-mac.sh
@@ -5,4 +5,4 @@ MY_PASSWORD=""
 set -eux
 cd ansible || exit
 
-ansible-playbook --step --skip-tags data,company --diff --extra-vars "ansible_python_interpreter=/usr/bin/python3 ansible_password=${MY_PASSWORD} ansible_become_password=${MY_PASSWORD}" --connection=local -i "127.0.0.1," main.yml | tee -a /var/tmp/ansible_my_workstation-local.log
+ansible-playbook --skip-tags data --diff --extra-vars "ansible_python_interpreter=/usr/bin/python3 ansible_password=${MY_PASSWORD} ansible_become_password=${MY_PASSWORD}" --connection=local -i "127.0.0.1," main.yml | tee -a /tmp/ansible_my_workstation-local.log
