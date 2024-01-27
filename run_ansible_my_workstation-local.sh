f52b5219e8cc647840c52eb33c7395740a06ccce
Petr Ruzicka <petr.ruzicka@gmail.com>
2024-01-27 18:41:12 +0100

    feat(gha): unify GHA - renovate, megalinter, markdown, and others

--- run_ansible_my_workstation-local.sh
+++ run_ansible_my_workstation-local.sh
@@ -2,4 +2,4 @@
 
 set -eux
 cd ansible || exit
-ansible-playbook --diff --connection=local -i "127.0.0.1," main.yml | tee /var/tmp/ansible_my_workstation-local.log
+ansible-playbook --diff --connection=local -i "127.0.0.1," main.yml | tee /var/tmp/ansible_my_workstation-local.log # DevSkim: ignore DS162092
--- run_ansible_my_workstation.sh
+++ run_ansible_my_workstation.sh
@@ -5,4 +5,4 @@ MY_USER="pruzicka"
 MY_PASSWORD=""
 
 cd ansible || exit
-ansible-playbook --diff --user="${MY_USER}" --extra-vars "ansible_password=${MY_PASSWORD} ansible_become_password=${MY_PASSWORD}" -i "${DESTINATION_IP}," main.yml
+ansible-playbook --diff --user="${MY_USER}" --extra-vars "ansible_password=${MY_PASSWORD} ansible_become_password=${MY_PASSWORD}" -i "${DESTINATION_IP}," main.yml # DevSkim: ignore DS162092
