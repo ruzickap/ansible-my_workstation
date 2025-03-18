e1fb11dd6bd34016c1b27034b252f09c4656091e
Petr Ruzicka <petr.ruzicka@gmail.com>
2025-03-18 08:36:50 +0100

    fix: fix issues with mc and add mc tag (#150)

--- run_ansible_my_workstation-local-mac.sh
+++ run_ansible_my_workstation-local-mac.sh
@@ -5,4 +5,4 @@ MY_PASSWORD=""
 set -eux
 cd ansible || exit
 
-ansible-playbook --skip-tags data --diff --extra-vars "ansible_python_interpreter=/usr/bin/python3 ansible_password=${MY_PASSWORD} ansible_become_password=${MY_PASSWORD}" --connection=local -i "127.0.0.1," main.yml | tee -a /tmp/ansible_my_workstation-local.log
+ansible-playbook --skip-tags data --tags mc --diff --extra-vars "ansible_python_interpreter=/usr/bin/python3 ansible_password=${MY_PASSWORD} ansible_become_password=${MY_PASSWORD}" --connection=local -i "127.0.0.1," main.yml | tee -a /tmp/ansible_my_workstation-local.log
